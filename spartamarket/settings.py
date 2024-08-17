efb60e6cbef3314b037d2954fdcced94edf41a71
leeyounghuncom <web3dao@kakao.com>
2024-08-17 18:51:05 +0900

    20240817

--- spartamarket/settings.py
+++ spartamarket/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'products'
+    'products',
+    'django.contrib.humanize',
+
 ]
 
 MIDDLEWARE = [
@@ -65,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'accounts.context_processors.profile_image',
+
             ],
         },
     },
